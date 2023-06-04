09b069dd6b50530c6963d238008a450916eeadbc
Filip <filipsel95@gmail.com>
2023-06-04 10:58:58 +0200

    New Commit. Male izmene u ovoj "Prodavnica aplikaciji", gde sam napravio GUI.

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,92 @@
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main implements ActionListener {
 
-        Scanner scanner = new Scanner(System.in); //"Scanner scanner = new Scanner(System.in)" ovo je metoda
+    private JFrame frame;
+    private JPanel panel;
+    private JLabel titleLabel;
+    private JLabel countLabel;
+    private JTextField countTextField;
+    private JButton submitButton;
 
-        System.out.println("Unesite broj artikala: ");
-        int brojArtikala = scanner.nextInt();
-        Artikal[] nizArtikala = new Artikal[brojArtikala];
-        for (int i = 0; i < nizArtikala.length; i++) {
-            System.out.println("Unesite zeljeni proizvod:");
-            String ime = scanner.next();
-            System.out.println("Unesite cenu proizvoda:");
-            double cena = scanner.nextDouble();
-            System.out.println("Unesite kolicinu proizvoda:");
-            int kolicina = scanner.nextInt();
+    public Main() {
+        frame = new JFrame("Online Bank Management System");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            Artikal artikal = new Artikal(ime, cena, kolicina);
-            nizArtikala[i] = artikal;
-        }
-        istampajRacun((nizArtikala));
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 1));
+
+        titleLabel = new JLabel("Unesite broj artikala:");
+        countLabel = new JLabel("Broj artikala:");
+        countTextField = new JTextField();
+        submitButton = new JButton("Unesi");
+
+        submitButton.addActionListener(this);
+
+        panel.add(titleLabel);
+        panel.add(countLabel);
+        panel.add(countTextField);
+        panel.add(submitButton);
+
+        frame.getContentPane().add(panel);
+        frame.pack();
+        frame.setVisible(true);
     }
 
-    public static <niz, max> void istampajRacun(Artikal[] niz) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == submitButton) {
+            String input = countTextField.getText();
+            int brojArtikala = Integer.parseInt(input);
+
+            Artikal[] nizArtikala = new Artikal[brojArtikala];
+
+            for (int i = 0; i < nizArtikala.length; i++) {
+                String ime = JOptionPane.showInputDialog(frame, "Unesite ime proizvoda:");
+                double cena = Double.parseDouble(JOptionPane.showInputDialog(frame, "Unesite cenu proizvoda:"));
+                int kolicina = Integer.parseInt(JOptionPane.showInputDialog(frame, "Unesite kolicinu proizvoda:"));
+
+                Artikal artikal = new Artikal(ime, cena, kolicina);
+                nizArtikala[i] = artikal;
+            }
+
+            istampajRacun(nizArtikala);
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Main();
+            }
+        });
+    }
+
+    public static void istampajRacun(Artikal[] niz) {
         double konacanZbir = 0;
-        double maxCena = 0; //"maxCena = 0" ovo je konstruktor
+        double maxCena = 0;
         double maxUkupno = 0;
+
         for (int i = 0; i < niz.length; i++) {
             Artikal artikal = niz[i];
             double ukupnaCena = artikal.izracunajUkupnuCenuZaArtikal();
             konacanZbir += ukupnaCena;
             artikal.stampajArtikal();
-            if (artikal.cena > maxCena) {
-                maxCena = artikal.cena;
+            if (artikal.getCena() > maxCena) {
+                maxCena = artikal.getCena();
             }
-            if (artikal.cena * artikal.kolicina * artikal.porez > maxUkupno) {
-                maxUkupno = artikal.cena * artikal.kolicina * artikal.porez;
+            if (artikal.getCena() * artikal.getKolicina() * artikal.getPorez() > maxUkupno) {
+                maxUkupno = artikal.getCena() * artikal.getKolicina() * artikal.getPorez();
             }
         }
-        System.out.println("Za uplatu: " + konacanZbir + " dinara");
-        System.out.println("Najveca cena artikla je " + maxCena + " dinara");
-        System.out.println("Najveca ukupna cena artikla je " + maxUkupno + " dinara");
+
+        String message = "Za uplatu: " + konacanZbir + " dinara"
+                + "\nNajveca cena artikla je " + maxCena + " dinara"
+                + "\nNajveca ukupna cena artikla je " + maxUkupno + " dinara";
+
+        JOptionPane.showMessageDialog(null, message);
     }
 }
